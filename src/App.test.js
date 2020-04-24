f21b69b8c0fae18893d950a7228b44812ed5b89d
IsaiahFlax <flax.isaiah@gmail.com>
2020-04-23 20:04:15 -0600

    a different test for Appjs, both wont work at the same time

--- src/App.test.js
+++ src/App.test.js
@@ -4,8 +4,19 @@ import {fetchShow as mockFetchShow} from './api/fetchShow';
 
 import App from './App';
 
-//jest.mock('./api/fetchShow')
+// test('App renders', ()=>{
+//     render(<App />);
+// })
 
-test('App renders', ()=>{
-    render(<App />);
+jest.mock('./api/fetchShow')
+
+test('async test', async ()=>{
+    mockFetchShow.mockResolvedValueOnce({data: {
+        _embedded: {
+            episodes: [{
+                id: 553946,
+                name: 'name'
+            }]
+        }
+    }})
 })
